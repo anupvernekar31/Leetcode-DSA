a2a11518ff6bd3974ef7812dc14aec1cb4577613
anupvernekar31 <43994265+anupvernekar31@users.noreply.github.com>
2025-01-05 11:27:32 +0530

    Time: 1 ms (100%), Space: 47.5 MB (70.39%) - LeetHub

--- 0966-binary-subarrays-with-sum/0966-binary-subarrays-with-sum.java
+++ 0966-binary-subarrays-with-sum/0966-binary-subarrays-with-sum.java
@@ -1,19 +1,37 @@
 class Solution {
-    public int numSubarraysWithSum(int[] nums, int goal) {
-        
-        int currentSum=0;
-        int count=0;
-        Map<Integer,Integer> map=new HashMap<>();
-        map.put(0,1);
-        for(int i=0;i<nums.length;i++){
-            currentSum+=nums[i];
-            int requiredn=currentSum-goal;
-            if(map.containsKey(requiredn)){
-                count+=map.get(requiredn);
+    int solution(int[] nums, int goal){
+        int l=0, r=0;
+        int count = 0;
+        int sum = 0;
+        while(r<nums.length){
+            sum+=nums[r];
+            while(sum>goal && l<=r){
+                sum-=nums[l];
+                l++;
             }
-            map.put(currentSum,map.getOrDefault(currentSum,0)+1);
+            count+= r-l+1;
+            r++;
+
         }
         return count;
+    }
+    public int numSubarraysWithSum(int[] nums, int goal) {
+        
+        // int currentSum=0;
+        // int count=0;
+        // Map<Integer,Integer> map=new HashMap<>();
+        // map.put(0,1);
+        // for(int i=0;i<nums.length;i++){
+        //     currentSum+=nums[i];
+        //     int requiredn=currentSum-goal;
+        //     if(map.containsKey(requiredn)){
+        //         count+=map.get(requiredn);
+        //     }
+        //     map.put(currentSum,map.getOrDefault(currentSum,0)+1);
+        // }
+        // return count;
+
+        return solution(nums, goal) - solution(nums, goal-1);
     
     }
 }
